0e81874be418c183d13d288342d60ff63cc4beab
matheusrpd <matheusrpd@gmail.com>
2022-01-06 17:46:03 -0300

    Adicionando Broadcast para comunicar signOut entre abas.

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,11 +5,14 @@ import { setupAPIClient } from "../services/api";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign Out</button>
+      
       <Can permissions={['metrics.list']}>
         <div>Métricas</div>
       </Can>
